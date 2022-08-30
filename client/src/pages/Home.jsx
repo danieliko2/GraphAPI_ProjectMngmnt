05966fd06ab0c4a950d44eb5c601663ad8c21848
danieliko2 <danielkohav2@gmail.com>
2022-08-31 00:14:59 +0300

    fix .2

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,13 +6,16 @@ import AddProjectModal from "../components/AddProjectModal";
 export default function home() {
   return (
     <>
-      <div className="d-flex gap-3 mb4">
-          <AddClientModal />
-          <AddProjectModal />
+      <div>
+        <div className="d-flex gap-3 mb4">
+            <AddClientModal />
+            <AddProjectModal />
+        </div>
+        <Projects />
+        <hr />
+        <Clients />
       </div>
-      <Projects />
-      <hr />
-      <Clients />
     </>
+    
   )
 }
